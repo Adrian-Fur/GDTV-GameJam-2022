48895232100bce6cb0fd8d30ba21047cb76a0135
Adrian Furmanek <adrian.furm@gmail.com>
2022-05-29 19:45:50 +0200

    volume script fix

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -34,30 +34,4 @@ public class VolumeController : MonoBehaviour
     {
         PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
-    // [SerializeField] Slider volumeSlider = null;
-    // [SerializeField] TextMeshProUGUI volumeText = null;
-
-    // private void Start() 
-    // {
-    //     LoadValues();
-    // }
-
-    // public void VolumeSlider(float volume)
-    // {
-    //     volumeText.text = volume.ToString("0.0");
-    // }
-
-    // public void SaveVolumeButton()
-    // {
-    //     float volumeValue = volumeSlider.value;
-    //     PlayerPrefs.SetFloat("VolumeValue", volumeValue);
-    //     LoadValues();
-    // }
-
-    // void LoadValues()
-    // {
-    //     float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
-    //     volumeSlider.value = volumeValue;
-    //     AudioListener.volume = volumeValue;
-    // }
 }
